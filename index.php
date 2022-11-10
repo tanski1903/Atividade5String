761ea966dd6568536ef460cd8da5da4064cd5f97
MatheusBrasil19 <matheusbrasil862@gmail.com>
2022-11-10 19:27:42 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+    <title>Atividade 5 Função String</title>
+</head>
+<body>
+    <Form action="atividade5.php" method="post">
+        <h1>Inverter palavras</h1>
+        <br><br>
+        <label for="p">Digite uma Palavra:</label>
+        <br><br>
+        <input type="text" id="p" name="p"/>
+        <br><br>
+        <input type="submit" value="Inverter">
+    </Form>
+</body>
+</html>
